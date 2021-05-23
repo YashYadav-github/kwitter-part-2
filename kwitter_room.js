00904d215626ad6228c0ad479a7647d19c28d7a1
YashYadav-github <74818709+YashYadav-github@users.noreply.github.com>
2021-05-23 10:57:58 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA705wiuXfe91-R7iN05PUcEqVayqPxzzg",
+    authDomain: "kwitter-project-74f1f.firebaseapp.com",
+    projectId: "kwitter-project-74f1f",
+    storageBucket: "kwitter-project-74f1f.appspot.com",
+    messagingSenderId: "777372874305",
+    appId: "1:777372874305:web:e0dfd3beb5b0ddafcef8c0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
